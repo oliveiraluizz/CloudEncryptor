bcb19ea6ad0a57d846132c0129c614d7c26bba7e
oliveiraluizz <oliv.luizreis@gmail.com>
2024-07-06 22:48:26 -0300

    Project Inicialization

---
+++ src/main/java/com/cloud/encripty/CloudEncripty/CloudEncriptyApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.encripty.CloudEncripty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEncriptyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEncriptyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/encripty/CloudEncripty/CloudEncriptyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.encripty.CloudEncripty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEncriptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
